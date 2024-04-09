36e2fecb96f66dc0a7129fb14625cec43d634f57
kssrao13882 <kssrao13882@gmail.com>
2024-04-09 06:25:39 +0530

    marble: Project Matrixx Official Bringup

--- lineage_marble.mk
+++ lineage_marble.mk
@@ -10,6 +10,7 @@ $(call inherit-product, device/xiaomi/marble/device.mk)
 # Inherit from common lineage configuration
 TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_NAME := lineage_marble
 PRODUCT_DEVICE := marble
@@ -27,4 +28,24 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := POCO/marble_global/marble:13/TKQ1.221022.001/V14.0.7.0.TMRMIXM:user/release-keys
 
+
+# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_GAPPS_ARCH := arm64
+
+# Matrixx
+MATRIXX_BUILD_TYPE := Official
+MATRIXX_MAINTAINER := KSSRAO
+MATRIXX_CHIPSET := SM7475
+MATRIXX_BATTERY := 5000mah
+MATRIXX_DISPLAY := 1080x2400
+
+# Gapps
+WITH_GMS := true
+BUILD_GOOGLE_MESSAGE := true
+
+# Nuke AudioFX
+TARGET_EXCLUDES_AUDIOFX := true
+
+# Nuke Auxio
+TARGET_EXCLUDES_AUXIO := true
